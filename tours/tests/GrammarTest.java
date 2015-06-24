6c20fde6b9830850db179269ad30e441c53ab805
Jip Spel <jipspel@gmail.com>
2015-06-24 22:43:22 +0200

    Made treetest more readable

--- tours/tests/GrammarTest.java
+++ tours/tests/GrammarTest.java
@@ -2,6 +2,7 @@ package tours.tests;
 
 
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.Test;
 import tours.grammar.ToursErrorListener;
 import tours.grammar.ToursLexer;
@@ -19,19 +20,19 @@ public class GrammarTest {
 
     @Test
     public void testTree() {
-        String result6 = "([] ([30] ([32 30] ([39 32 30] ([46 39 32 30] integer) x , y) ;)))";
+        String result6 = "(program (body (variableDeclaration (variable (variableType integer) x , y) ;)))";
         assertEquals(result6, getParseTree("tours/tests/examples/example6.tours"));
 
-        String result7 = "([] ([30] ([33 30] main ( ) ([62 33 30] { ([65 62 33 30] ([77 65 62 33 30] x) = ([79 65 62 33 30] 35)) ; }))))";
+        String result7 = "(program (body (function main ( ) (block { (statement (identifier x) = (expression 35)) ; }))))";
         assertEquals(result7, getParseTree("tours/tests/examples/example7.tours"));
 
-        String result8 = "([] ([30] ([33 30] main ( ) ([62 33 30] { ([65 62 33 30] if ( ([83 65 62 33 30] ([16 83 65 62 33 30] x) ([165 83 65 62 33 30] !=) ([166 83 65 62 33 30] 35)) ) ([85 65 62 33 30] { ([65 85 65 62 33 30] ([77 65 85 65 62 33 30] x) = ([79 65 85 65 62 33 30] 35)) ; })) ; }))))";
+        String result8 = "(program (body (function main ( ) (block { (statement if ( (expression (expression x) (compareOperator !=) (expression 35)) ) (block { (statement (identifier x) = (expression 35)) ; })) ; }))))";
         assertEquals(result8, getParseTree("tours/tests/examples/example8.tours"));
 
-        String result9 = "([] ([30] ([33 30] main ( ) ([62 33 30] { ([65 62 33 30] while ( ([92 65 62 33 30] ([16 92 65 62 33 30] x) ([165 92 65 62 33 30] !=) ([166 92 65 62 33 30] 35)) ) ([94 65 62 33 30] { ([65 94 65 62 33 30] ([77 65 94 65 62 33 30] x) = ([79 65 94 65 62 33 30] ([16 79 65 94 65 62 33 30] x) ([157 79 65 94 65 62 33 30] +) ([158 79 65 94 65 62 33 30] 1))) ; })) ; }))))";
+        String result9 = "(program (body (function main ( ) (block { (statement while ( (expression (expression x) (compareOperator !=) (expression 35)) ) (block { (statement (identifier x) = (expression (expression x) (plusOperator +) (expression 1))) ; })) ; }))))";
         assertEquals(result9, getParseTree("tours/tests/examples/example9.tours"));
 
-        String result10 = "([] ([30] ([33 30] main ( ) ([62 33 30] { ([65 62 33 30] for ( ([99 65 62 33 30] ([77 99 65 62 33 30] x) = ([79 99 65 62 33 30] 1)) ; ([103 65 62 33 30] ([16 103 65 62 33 30] x) ([165 103 65 62 33 30] <) ([166 103 65 62 33 30] 35)) ; ([105 65 62 33 30] ([77 105 65 62 33 30] x) = ([79 105 65 62 33 30] ([16 79 105 65 62 33 30] x) ([157 79 105 65 62 33 30] +) ([158 79 105 65 62 33 30] 1))) ) ([107 65 62 33 30] { ([65 107 65 62 33 30] ([77 65 107 65 62 33 30] i) = ([79 65 107 65 62 33 30] 35)) ; })) ; }))))";
+        String result10 = "(program (body (function main ( ) (block { (statement for ( (statement (identifier x) = (expression 1)) ; (expression (expression x) (compareOperator <) (expression 35)) ; (statement (identifier x) = (expression (expression x) (plusOperator +) (expression 1))) ) (block { (statement (identifier i) = (expression 35)) ; })) ; }))))";
         assertEquals(result10, getParseTree("tours/tests/examples/example10.tours"));
     }
 
@@ -89,18 +90,18 @@ public class GrammarTest {
             return "Failed to load file %s".format(filename);
         }
 
-        CharStream chars = new ANTLRInputStream(file);
-
-        ToursErrorListener errorListener = new ToursErrorListener();
-        Lexer lexer = new ToursLexer(chars);
-        lexer.removeErrorListeners();
-        lexer.addErrorListener(errorListener);
+        // Convert the input text to a character stream
+        CharStream stream = new ANTLRInputStream(file);
+        // Build a lexer on top of the character stream
+        Lexer lexer = new ToursLexer(stream);
+        // Extract a token stream from the lexer
         TokenStream tokens = new CommonTokenStream(lexer);
+        // Build a parser instance on top of the token stream
         ToursParser parser = new ToursParser(tokens);
-        parser.removeErrorListeners();
-        parser.addErrorListener(errorListener);
-        String tree = parser.program().toStringTree();
-        return errorListener.getErrorList().size() > 0 ? "[]" : tree;
+        // Get the parse tree by calling the start rule
+        ParseTree tree = parser.program();
+
+        return tree.toStringTree(parser);
     }
 
     private String read(String filename) {
