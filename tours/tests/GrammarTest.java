7e7dece61ab1dc8bec5cb89ad3daf6e07d11b1bd
Jip Spel <jipspel@gmail.com>
2015-06-24 16:34:28 +0200

    Added test for trees

--- tours/tests/GrammarTest.java
+++ tours/tests/GrammarTest.java
@@ -18,6 +18,24 @@ import static org.junit.Assert.assertEquals;
 public class GrammarTest {
 
     @Test
+    public void testTree() {
+        String result6 = "([] ([30] ([32 30] ([39 32 30] ([46 39 32 30] integer) x , y) ;)))";
+        assertEquals(result6, getParseTree("tours/tests/examples/example6.tours"));
+
+        String result7 = "([] ([30] ([33 30] main ( ) ([62 33 30] { ([65 62 33 30] ([77 65 62 33 30] x) = ([79 65 62 33 30] 35)) ; }))))";
+        assertEquals(result7, getParseTree("tours/tests/examples/example7.tours"));
+
+        String result8 = "([] ([30] ([33 30] main ( ) ([62 33 30] { ([65 62 33 30] if ( ([83 65 62 33 30] ([16 83 65 62 33 30] x) ([165 83 65 62 33 30] !=) ([166 83 65 62 33 30] 35)) ) ([85 65 62 33 30] { ([65 85 65 62 33 30] ([77 65 85 65 62 33 30] x) = ([79 65 85 65 62 33 30] 35)) ; })) ; }))))";
+        assertEquals(result8, getParseTree("tours/tests/examples/example8.tours"));
+
+        String result9 = "([] ([30] ([33 30] main ( ) ([62 33 30] { ([65 62 33 30] while ( ([92 65 62 33 30] ([16 92 65 62 33 30] x) ([165 92 65 62 33 30] !=) ([166 92 65 62 33 30] 35)) ) ([94 65 62 33 30] { ([65 94 65 62 33 30] ([77 65 94 65 62 33 30] x) = ([79 65 94 65 62 33 30] ([16 79 65 94 65 62 33 30] x) ([157 79 65 94 65 62 33 30] +) ([158 79 65 94 65 62 33 30] 1))) ; })) ; }))))";
+        assertEquals(result9, getParseTree("tours/tests/examples/example9.tours"));
+
+        String result10 = "([] ([30] ([33 30] main ( ) ([62 33 30] { ([65 62 33 30] for ( ([99 65 62 33 30] ([77 99 65 62 33 30] x) = ([79 99 65 62 33 30] 1)) ; ([103 65 62 33 30] ([16 103 65 62 33 30] x) ([165 103 65 62 33 30] <) ([166 103 65 62 33 30] 35)) ; ([105 65 62 33 30] ([77 105 65 62 33 30] x) = ([79 105 65 62 33 30] ([16 79 105 65 62 33 30] x) ([157 79 105 65 62 33 30] +) ([158 79 105 65 62 33 30] 1))) ) ([107 65 62 33 30] { ([65 107 65 62 33 30] ([77 65 107 65 62 33 30] i) = ([79 65 107 65 62 33 30] 35)) ; })) ; }))))";
+        assertEquals(result10, getParseTree("tours/tests/examples/example10.tours"));
+    }
+
+    @Test
     public void testErrorListSize() {
         List<String> errorList;
 
@@ -43,11 +61,8 @@ public class GrammarTest {
     }
 
     private List<String> parseToursFile(String filename) {
-        String file;
-
-        try {
-            file = new String(readAllBytes(get(filename)));
-        } catch (IOException e) {
+        String file = read(filename);
+        if (file == "") {
             List<String> errorList = new ArrayList<>();
             errorList.add("Failed to load file %s".format(filename));
             return errorList;
@@ -65,5 +80,36 @@ public class GrammarTest {
         parser.addErrorListener(errorListener);
         parser.program();
         return errorListener.getErrorList();
+    }
+
+    private String getParseTree(String filename){
+        String file = read(filename);
+
+        if (file == "") {
+            return "Failed to load file %s".format(filename);
+        }
+
+        CharStream chars = new ANTLRInputStream(file);
+
+        ToursErrorListener errorListener = new ToursErrorListener();
+        Lexer lexer = new ToursLexer(chars);
+        lexer.removeErrorListeners();
+        lexer.addErrorListener(errorListener);
+        TokenStream tokens = new CommonTokenStream(lexer);
+        ToursParser parser = new ToursParser(tokens);
+        parser.removeErrorListeners();
+        parser.addErrorListener(errorListener);
+        String tree = parser.program().toStringTree();
+        return errorListener.getErrorList().size() > 0 ? "[]" : tree;
+    }
+
+    private String read(String filename) {
+        String file;
+        try {
+            file = new String(readAllBytes(get(filename)));
+        } catch (IOException e) {
+            file = "";
+        }
+        return file;
     }
 }
