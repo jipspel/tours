4fe4c64ded42c90783694f5d0ef4d79e2e27f3a0
Twan Coenraad <twan@coenraad.at>
2015-06-29 18:40:04 +0200

    Refactored CompilerTest to be platform-independent

--- src/test/java/tours/CompilerTest.java
+++ src/test/java/tours/CompilerTest.java
@@ -8,6 +8,8 @@ import tours.compiler.CompilerTools;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static java.nio.file.Files.readAllBytes;
 import static java.nio.file.Paths.get;
@@ -26,46 +28,58 @@ public class CompilerTest {
     }
 
     @Test
-    public void testHelloWorld() throws IOException, InterruptedException {
-        assertEquals("Hello World\n", compileAndRun("src/test/java/tours/examples/hello_world.tours"));
+    public void testHelloWorld() {
+        assertEqualsOutput(Arrays.asList("Hello World"), "src/test/java/tours/examples/hello_world.tours");
+    }
+
+    private void assertEqualsOutput(List<String> expectedOutput, String filename) {
+        String expectedOutputString = "";
+        for (String line : expectedOutput) {
+            expectedOutputString += line + System.getProperty("line.separator");
+        }
+        try {
+            assertEquals(expectedOutputString, compileAndRun(filename));
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void testAssignment() throws IOException, InterruptedException {
-        assertEquals("true\nfalse\nfalse\n", compileAndRun("src/test/java/tours/examples/simple_assignments_boolean_with_print.tours"));
-        assertEquals("a\nb\nc\n", compileAndRun("src/test/java/tours/examples/simple_assignments_character_with_print.tours"));
-        assertEquals("35\n36\n37\n", compileAndRun("src/test/java/tours/examples/simple_assignments_integer_with_print.tours"));
-        assertEquals("35\n36\n37\n", compileAndRun("src/test/java/tours/examples/simple_assignments_string_with_print.tours"));
+        assertEqualsOutput(Arrays.asList("true", "false", "false"), "src/test/java/tours/examples/simple_assignments_boolean_with_print.tours");
+        assertEqualsOutput(Arrays.asList("a", "b", "c"), "src/test/java/tours/examples/simple_assignments_character_with_print.tours");
+        assertEqualsOutput(Arrays.asList("35", "36", "37"), "src/test/java/tours/examples/simple_assignments_integer_with_print.tours");
+        assertEqualsOutput(Arrays.asList("35", "36", "37"), "src/test/java/tours/examples/simple_assignments_string_with_print.tours");
     }
 
     @Test
     public void testArithmeticExpressions() throws IOException, InterruptedException {
-        assertEquals("35\n36\n70\n35\n35\n", compileAndRun("src/test/java/tours/examples/arithmetic_expressions.tours"));
-        assertEquals("35\n36\n35\n", compileAndRun("src/test/java/tours/examples/arithmetic_expressions_order.tours"));
+        assertEqualsOutput(Arrays.asList("35", "36", "70", "35", "35"), "src/test/java/tours/examples/arithmetic_expressions.tours");
+        assertEqualsOutput(Arrays.asList("35", "36", "35"), "src/test/java/tours/examples/arithmetic_expressions_order.tours");
     }
 
     @Test
     public void testPrefixExpressions() throws IOException, InterruptedException {
-        assertEquals("35\n-36\nfalse\ntrue\n", compileAndRun("src/test/java/tours/examples/prefix_expressions.tours"));
+        assertEqualsOutput(Arrays.asList("35", "-36", "false", "true"), "src/test/java/tours/examples/prefix_expressions.tours");
     }
 
     @Test
     public void testBooleanExpressions() throws IOException, InterruptedException {
-        assertEquals("true\nfalse\nfalse\nfalse\n" +
-                "true\ntrue\ntrue\nfalse\n", compileAndRun("src/test/java/tours/examples/boolean_expressions.tours"));
-        assertEquals("true\ntrue\nfalse\n", compileAndRun("src/test/java/tours/examples/boolean_expressions_order.tours"));
+        assertEqualsOutput(Arrays.asList("true", "false", "false", "false", "true", "true", "true", "false"),
+                "src/test/java/tours/examples/boolean_expressions.tours");
+        assertEqualsOutput(Arrays.asList("true", "true", "false"), "src/test/java/tours/examples/boolean_expressions_order.tours");
     }
 
     @Test
     public void testCompareExpressions() throws IOException, InterruptedException {
-        assertEquals("true\nfalse\nfalse\n" +
-                "true\ntrue\nfalse\n" +
-                "false\nfalse\ntrue\n" +
-                "false\ntrue\ntrue\n" +
-                "true\ntrue\ntrue\n" +
-                "false\nfalse\nfalse\n" +
-                "false\nfalse\nfalse\n" +
-                "true\ntrue\ntrue\n", compileAndRun("src/test/java/tours/examples/compare_expressions.tours"));
+        assertEqualsOutput(Arrays.asList("true", "false", "false",
+                "true", "true", "false",
+                "false", "false", "true",
+                "false", "true", "true",
+                "true", "true", "true",
+                "false", "false", "false",
+                "false", "false", "false",
+                "true", "true", "true"), "src/test/java/tours/examples/compare_expressions.tours");
     }
 
     private String compileAndRun(String filename) throws IOException, InterruptedException {
